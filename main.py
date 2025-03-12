7a8d9652dbd24fd244896f73ecaa18cf2a747cc9
explorer2005 <dhruvguptaofficial2005@gmail.com>
2025-03-12 15:44:03 +0530

    fixed using google recognize

--- main.py
+++ main.py
@@ -8,23 +8,44 @@ engine = pyttsx3.init() #initialising text to  speech  engine
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+
+def processCommand(c):
+    pass
+
 if __name__ == "__main__" :
     speak("Initializing Jarvis......")
     #listen for the wake word Jarvis 
     while True:
         # obtain audio from the microphone
         r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Listening.....")
-            audio = r.listen(source)
+       
+        # # recognize speech using Sphinx
+        # try:
+        #     print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+        # except sr.UnknownValueError:
+        #     print("Sphinx could not understand audio")
+        # except sr.RequestError as e:
+        #     print("Sphinx error; {0}".format(e))
 
-        command = r.recognize_sphinx(audio)
-        print(command)
+        # recognize speech using google cloud
 
-        # recognize speech using Sphinx
+
+        print("Recognizing.....")
         try:
-            print("Sphinx thinks you said " + r.recognize_sphinx(audio))
-        except sr.UnknownValueError:
-            print("Sphinx could not understand audio")
-        except sr.RequestError as e:
-            print("Sphinx error; {0}".format(e))
+            with sr.Microphone() as source:
+                print("Listening.....")
+                audio = r.listen(source,timeout=2,phrase_time_limit=2)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                speak("Yeah baby tell what happened !!")
+                ##Listen for command
+                with sr.Microphone() as source:
+                    print("Jarvis is active...")
+                    audio = r.listen(source,phrase_time_limit=2)
+                    command = r.recognize_google(audio)
+                    processCommand()
+
+        # except sr.UnknownValueError:
+        #     print("Sphinx could not understand audio")
+        except Exception as e:
+            print("Not recognizing...")
